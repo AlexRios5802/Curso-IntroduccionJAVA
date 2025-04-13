9c2788bb12435d90061ba7d4c70d13157230e17b
Alex Rios <al066668@uacam.mx>
2025-04-13 00:06:43 -0600

    Subimos los dos proyectos del inicio del curso

---
+++ src/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Nombre completo: Alex Omar Ríos Pérez");
+        System.out.println("Edad: 23 años");
+        System.out.println("País: México");
+    }
+}
